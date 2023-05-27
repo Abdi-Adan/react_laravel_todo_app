98bc212f09b530c4697167028b7228f8b737e71b
abdi-adan <adanabdi036@gmail.com>
2023-05-28 02:25:10 +0300

    create markup for login, signup and default layout scaffolding

--- todoapp/src/components/DefaultLayout.jsx
+++ todoapp/src/components/DefaultLayout.jsx
@@ -1,16 +1,39 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
 
-export default function DefaultLayout(){
-    const {user, token} = useStateContext();
+export default function DefaultLayout() {
+    const { user, token } = useStateContext();
 
-    if(!token){
+    if (!token) {
         return <Navigate to="/login" />;
     }
 
+    const onLogout = (ev) => {
+        ev.preventDefault()
+    }
+
     return (
-        <div>
-            <Outlet />
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/admin">Admin</Link>
+                <Link to="/todo">Todo Items</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+
+
+                    <div>
+                        {user.name} &nbsp; &nbsp;
+                        <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
         </div>
     )
 }
--- todoapp/src/pages/Signup.jsx
+++ todoapp/src/pages/Signup.jsx
@@ -1,7 +1,24 @@
-export default function Signup(){
+import { Link } from "react-router-dom";
+
+export default function Signup() {
+    const onSubmit = ev => {
+        ev.preventDefault()
+    }
+
     return (
-        <div>
-            Signup
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Create an account</h1>
+                    <input type="text" placeholder="Full Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password confirmation" />
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">Already registered? <Link to="/login">Login</Link></p>
+
+                </form>
+            </div>
         </div>
     )
 }
