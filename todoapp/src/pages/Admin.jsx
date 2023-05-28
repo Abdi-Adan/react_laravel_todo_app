5818bc9b87e12e2a0951b65b0bc996671c64a631
abdi-adan <adanabdi036@gmail.com>
2023-05-28 05:15:36 +0300

    displaying all users on admin panel

--- todoapp/src/pages/Admin.jsx
+++ todoapp/src/pages/Admin.jsx
@@ -1,7 +1,66 @@
-export default function Admin(){
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client.js";
+import { Link } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider.jsx";
+
+export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({ data }) => {
+                setLoading(false)
+                setUsers(data.data)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
+
     return (
         <div>
-            Users
+            <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center" }}>
+                <h1>Users</h1>
+            </div>
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Create Date</th>
+                        </tr>
+                    </thead>
+                    {loading &&
+                        <tbody>
+                            <tr>
+                                <td colSpan="5" className="text-center">
+                                    Loading...
+                                </td>
+                            </tr>
+                        </tbody>
+                    }
+                    {!loading &&
+                        <tbody>
+                            {users.map(u => (
+                                <tr key={u.id}>
+                                    <td>{u.id}</td>
+                                    <td>{u.name}</td>
+                                    <td>{u.email}</td>
+                                    <td>{u.created_at}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    }
+                </table>
+            </div>
         </div>
     )
 }
