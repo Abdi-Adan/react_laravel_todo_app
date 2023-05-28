9fb6a66b4a79cfc10521004e2c25883178e1aade
abdi-adan <adanabdi036@gmail.com>
2023-05-28 04:46:28 +0300

    Complete auth; signup,login and logout

--- todoapp/src/components/DefaultLayout.jsx
+++ todoapp/src/components/DefaultLayout.jsx
@@ -1,16 +1,31 @@
-import { Link, Outlet, Navigate } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
+import axiosClient from "../axios-client.js";
+import { useEffect } from "react";
 
 export default function DefaultLayout() {
-    const { user, token } = useStateContext();
+    const { user, token, setUser, setToken } = useStateContext();
 
     if (!token) {
         return <Navigate to="/login" />;
     }
 
-    const onLogout = (ev) => {
+    const onLogout = ev => {
         ev.preventDefault()
+
+        axiosClient.post('/logout')
+            .then(() => {
+                setUser({})
+                setToken(null)
+            })
     }
+
+    useEffect(() => {
+        axiosClient.get('/user')
+            .then(({ data }) => {
+                setUser(data)
+            })
+    }, [])
 
     return (
         <div id="defaultLayout">
